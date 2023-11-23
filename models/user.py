2d779e4fff3292bb7278230acdfaff9289134fe3
benlaguroun <benlaguroun@gmail.com>
2023-11-22 23:49:39 -0800

    user

--- models/user.py
+++ models/user.py
@@ -8,7 +8,14 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel):
 
-    """ This class defines a user by various attributes """
+     """This is the class for user
+    Attributes:
+        __tablename__: Table name
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
