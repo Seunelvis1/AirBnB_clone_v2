8c43053f770c66df2d82dfbf966beeedee0df64d
benlaguroun <benlaguroun@gmail.com>
2024-01-01 11:10:10 -0800

    task0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,15 @@
-ets up a web server for deployment of web_static.
-
-sudo apt-get update
-sudo apt-get install -y nginx
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" | sudo tee /etc/nginx/sites-available/default
-
-sudo service nginx restart
+#!/usr/bin/env bash
+# setup server for deployment web_static
+apt-get update -y
+apt-get upgrade -y
+apt-get install nginx -y
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+echo "test deploying web_static" > /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+service nginx restart
 
